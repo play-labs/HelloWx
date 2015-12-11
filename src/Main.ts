e42c14b33f025f925414c98a654f4f9498e44e2b
liutianpeng <liutp@163.com>
2015-12-12 01:35:00 +0800

    init

---
+++ src/Main.ts
@@ -0,0 +1,254 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-2015, Egret Technology Inc.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+class Main extends eui.UILayer {
+    /**
+     * 加载进度界面
+     * loading process interface
+     */
+    private loadingView: LoadingUI;
+    protected createChildren(): void {
+        super.createChildren();
+        //inject the custom material parser
+        //注入自定义的素材解析器
+        var assetAdapter = new AssetAdapter();
+        this.stage.registerImplementation("eui.IAssetAdapter",assetAdapter);
+        this.stage.registerImplementation("eui.IThemeAdapter",new ThemeAdapter());
+        //Config loading process interface
+        //设置加载进度界面
+        this.loadingView = new LoadingUI();
+        this.stage.addChild(this.loadingView);
+        // initialize the Resource loading library
+        //初始化Resource资源加载库
+        RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.loadConfig("resource/default.res.json", "resource/");
+    }
+    /**
+     * 配置文件加载完成,开始预加载皮肤主题资源和preload资源组。
+     * Loading of configuration file is complete, start to pre-load the theme configuration file and the preload resource group
+     */
+    private onConfigComplete(event:RES.ResourceEvent):void {
+        RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        // load skin theme configuration file, you can manually modify the file. And replace the default skin.
+        //加载皮肤主题配置文件,可以手动修改这个文件。替换默认皮肤。
+        var theme = new eui.Theme("resource/default.thm.json", this.stage);
+        theme.addEventListener(eui.UIEvent.COMPLETE, this.onThemeLoadComplete, this);
+
+        RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+        RES.loadGroup("preload");
+    }
+    private isThemeLoadEnd: boolean = false;
+    /**
+     * 主题文件加载完成,开始预加载
+     * Loading of theme configuration file is complete, start to pre-load the 
+     */
+    private onThemeLoadComplete(): void {
+        this.isThemeLoadEnd = true;
+        this.createScene();
+    }
+    private isResourceLoadEnd: boolean = false;
+    /**
+     * preload资源组加载完成
+     * preload resource group is loaded
+     */
+    private onResourceLoadComplete(event:RES.ResourceEvent):void {
+        if (event.groupName == "preload") {
+            this.stage.removeChild(this.loadingView);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+            this.isResourceLoadEnd = true;
+            this.createScene();
+        }
+    }
+    private createScene(){
+        if(this.isThemeLoadEnd && this.isResourceLoadEnd){
+            this.startCreateScene();
+        }
+    }
+    /**
+     * 资源组加载出错
+     * Resource group loading failed
+     */
+    private onResourceLoadError(event:RES.ResourceEvent):void {
+        //TODO
+        console.warn("Group:" + event.groupName + " has failed to load");
+        //忽略加载失败的项目
+        //ignore loading failed projects
+        this.onResourceLoadComplete(event);
+    }
+    /**
+     * preload资源组加载进度
+     * loading process of preload resource
+     */
+    private onResourceProgress(event:RES.ResourceEvent):void {
+        if (event.groupName == "preload") {
+            this.loadingView.setProgress(event.itemsLoaded, event.itemsTotal);
+        }
+    }
+    /**
+     * 创建场景界面
+     * Create scene interface
+     */
+    protected startCreateScene(): void {
+        
+        var bodyConfig: BodyConfig = new BodyConfig();
+        bodyConfig.appId = egret["wxAppId"];
+        bodyConfig.timestamp = egret["wxTimeStamp"];
+        bodyConfig.nonceStr = egret["wxNonce"];
+        bodyConfig.signature = egret["wxSignature"];
+        bodyConfig.jsApiList = [
+            'checkJsApi',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage',
+            'onMenuShareQQ',
+            'onMenuShareWeibo',
+            'hideMenuItems',
+            'showMenuItems',
+            'hideAllNonBaseMenuItem',
+            'showAllNonBaseMenuItem',
+            'translateVoice',
+            'startRecord',
+            'stopRecord',
+            'onRecordEnd',
+            'playVoice',
+            'pauseVoice',
+            'stopVoice',
+            'uploadVoice',
+            'downloadVoice',
+            'chooseImage',
+            'previewImage',
+            'uploadImage',
+            'downloadImage',
+            'getNetworkType',
+            'openLocation',
+            'getLocation',
+            'hideOptionMenu',
+            'showOptionMenu',
+            'closeWindow',
+            'scanQRCode',
+            'chooseWXPay',
+            'openProductSpecificView',
+            'addCard',
+            'chooseCard',
+            'openCard'
+        ];
+        
+        bodyConfig.debug = true;
+        if(wx) {
+            /*
+            alert(bodyConfig.appId)
+            alert(bodyConfig.nonceStr)
+            alert(bodyConfig.timestamp)
+            alert(bodyConfig.signature)
+                */
+                
+            wx.config(bodyConfig);
+            wx.ready(function() {
+                //分享好友
+                var msg: BodyMenuShareAppMessage = new BodyMenuShareAppMessage;                
+                msg.title = "Egret的微信分享好友"
+                msg.desc = "Egret的微信分享好使不"
+                msg.link = egret["wxShareURL"];
+                msg.imgUrl = egret["wxPic"];
+                msg.type = "link";
+                msg.dataUrl = "";
+                msg.success = function() { }
+                msg.cancel = function() { }
+                wx.onMenuShareAppMessage(msg);
+                
+                //分享朋友圈
+                var tl: BodyMenuShareTimeline = new BodyMenuShareTimeline;
+                tl.title = "Egret的微信分享朋友圈"
+                tl.link = egret["wxShareURL"];
+                tl.imgUrl = egret["wxPic"];
+                tl.success = function() { }
+                tl.cancel = function() { }
+                wx.onMenuShareTimeline(tl);
+            });
+        }
+        
+        
+        var button = new eui.Button();
+        button.label = "Click!";
+        button.horizontalCenter = 0;
+        button.verticalCenter = 0;
+        this.addChild(button);
+        button.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+    }
+
+    private onButtonClick(e: egret.TouchEvent) {
+        egret["wxShareTitle"] = "从egret里面写ShareTitle"
+        var panel = new eui.Panel();
+        panel.title = egret["wxToken"];
+        panel.horizontalCenter = 0;
+        panel.verticalCenter = 0;
+        
+        var lableUser = new eui.Label();
+        lableUser.x = 20;
+        lableUser.y = 60;
+        lableUser.background = true;
+        lableUser.backgroundColor = 0xFF0F0FFF;
+        lableUser.text = "你:" + egret["wxUserName"];
+        panel.addChild(lableUser);
+        
+        var lableShare = new eui.Label();
+        lableShare.text = "邀请:" + egret["wxShareName"];
+        lableShare.x = 20;
+        lableShare.y = 120;
+        lableShare.background = true;
+        lableShare.backgroundColor = 0xFF0F0FFF;
+        panel.addChild(lableShare);
+        
+        var wxPix = new eui.Image();
+        var completFun = function(data: any): void {
+            console.debug("就是这里");
+            var pic: egret.Bitmap = new egret.Bitmap(data);
+            wxPix.width = 100;
+            wxPix.height = 100;
+            wxPix.x = 300;
+            wxPix.y = 60;
+            wxPix.fillMode = egret.BitmapFillMode.SCALE;
+            wxPix.source = pic.texture
+            panel.addChild(wxPix);
+        };
+        RES.getResByUrl(egret["wxPic"], completFun, null);
+        
+        this.addChild(panel);
+    }
+    
+    public getWxPixComplete(data: any): void {
+        var head: egret.Bitmap = new egret.Bitmap(data);
+        head.anchorOffsetX = 0.5;
+        head.anchorOffsetY = 0.5;
+        this.addChild(head);
+    }
+}
